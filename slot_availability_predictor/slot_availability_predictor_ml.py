a8b5796415b4e4fdb68e088083c562b7d68c50b8
Alex Costa <costa98alex@gmail.com>
2022-07-13 19:46:36 +0200

    hello

--- slot_availability_predictor/slot_availability_predictor_ml.py
+++ slot_availability_predictor/slot_availability_predictor_ml.py
@@ -87,29 +87,3 @@ if __name__ == "__main__":
 
     mqttServer.clientMQTT.publish(PRICE_TOPIC, '{:f}'.format(current_price), qos=QOS, retain=True)
 
-    #4.0 model creation
-    my_model = Prophet(interval_width=0.95, weekly_seasonality=True)
-
-
-    #5.0 fit the data
-    my_model.fit(df)
-
-    #6.0 creation of future dataframe
-    future_dates = my_model.make_future_dataframe(periods=36, freq='MS')
-    print(future_dates.tail())
-
-    #7.0 forecast
-    forecast = my_model.predict(future_dates)
-    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-
-    #8.0 plot of the forecast
-    plt2 = my_model.plot(forecast, uncertainty=True)
-    plt2.show()
-    fzwait()
-
-
-
-    plt3 = my_model.plot_components(forecast)
-    plt3.show()
-    fzwait()
-
